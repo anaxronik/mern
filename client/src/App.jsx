c42995413b1f17ffb575abe0ba0d00c4811c6297
Sergey Komarov <anaxronik@gmail.com>
2020-03-24 02:00:01 +0500

    Create and connect routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import 'materialize-css'
+import { useRoutes } from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
+  const routes = useRoutes(true)
   return (
-    <div className='container'>
-      <h1>REACT APP WORK</h1>
-    </div>
+    <BrowserRouter>
+      <div className='container'>
+        <h1>REACT APP WORK</h1>
+        {routes}
+      </div>
+    </BrowserRouter>
   );
 }
 
