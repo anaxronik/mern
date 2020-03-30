9d9543a1215ee68af87e8a6a52cd6381c31af3ed
Sergey Komarov <anaxronik@gmail.com>
2020-03-30 15:29:22 +0500

    refactor

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -7,37 +7,38 @@ const AuthPage = () => {
                 <h2>Сокращение ссылок</h2>
 
                 <div className="card blue darken-1">
-                    <div className="card-content white-text">
-                        <span className="card-title">Авторизация</span>
+                    <form action="">
+                        <div className="card-content white-text">
+                            <span className="card-title">Авторизация</span>
 
-                        <div>
-                            <div class="input-field">
-                                <input
-                                    placeholder="Enter your email"
-                                    id="email"
-                                    type="text"
-                                    name='email'
-                                />
-                                <label htmlFor="email">Your email</label>
+                            <div>
+                                <div className="input-field">
+                                    <input
+                                        placeholder="Enter your email"
+                                        id="email"
+                                        type="text"
+                                        name='email'
+                                    />
+                                    <label htmlFor="email">Your email</label>
+                                </div>
+                                <div className="input-field">
+                                    <input
+                                        placeholder="Enter your password"
+                                        id="password"
+                                        type="password"
+                                        name='email'
+                                    />
+                                    <label htmlFor="password">Your password</label>
+                                </div>
                             </div>
-                            <div class="input-field">
-                                <input
-                                    placeholder="Enter your password"
-                                    id="password"
-                                    type="password"
-                                    name='email'
-                                    className='yellow-input'
-                                />
-                                <label htmlFor="password">Your password</label>
-                            </div>
+
                         </div>
+                        <div className="card-action">
+                            <button className='btn yellow darken-4' style={{ marginRight: 10 }}> Войти</button>
+                            <button className='btn grey lighten-1 black-text'> Регистрация</button>
 
-                    </div>
-                    <div className="card-action">
-                        <button className='btn yellow darken-4' style={{ marginRight: 10 }}> Войти</button>
-                        <button className='btn grey lighten-1 black-text'> Регистрация</button>
-
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
