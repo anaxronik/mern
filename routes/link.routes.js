00f0dc1c94c8607c9ae4a8168d87e3cab47e2f34
Sergey Komarov <anaxronik@gmail.com>
2020-03-31 15:25:45 +0500

    add create short backend

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -2,9 +2,30 @@ const { Router } = require('express')
 const Link = require('../models/Link')
 const router = Router()
 const auth = require('../midleware/auth.midleware')
+const config = require('config')
+const shortid = require('shortid')
 
 router.post('/generate', async (req, res) => {
     try {
+        const baseUrl = config.get('baseUrl')
+        const { from } = req.body
+
+        const code = shortid.generate()
+
+        const existing = await Link.findOne({ from })
+
+        if (existing) {
+            return res.json({ link: existing })
+        }
+
+        const to = baseUrl + '/t/' + code
+
+        const link = new Link({ code, to, from, owner: req.user.userId })
+
+        await link.save()
+
+        res.status(201).json({ link })
+
 
     } catch (error) {
         res.status(500).json({ message: 'что-то пошло не так, попробуйте снова' })
