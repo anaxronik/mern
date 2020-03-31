6af76e7d25db0eeb14b3e6250ef6b60891313fdc
Sergey Komarov <anaxronik@gmail.com>
2020-03-31 13:47:25 +0500

    add context

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useHttp } from '../hooks/http.hook';
 import { useMessage } from '../hooks/message.hook';
+import { AuthContext } from '../context/AuthContext';
 
 
 const AuthPage = () => {
+    const auth = useContext(AuthContext)
     const message = useMessage()
     const { loading, error, request, clearError } = useHttp()
     const [form, setForm] = useState({ email: '', password: '', });
