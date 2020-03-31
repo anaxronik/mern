06a8cdca7721e27c48e317abdcfa5a76bc2ba81d
Sergey Komarov <anaxronik@gmail.com>
2020-04-01 01:06:23 +0500

    complete detail link page

--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useContext, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import LinkCard from '../components/LinkCard'
+import { useState } from 'react';
+import { useHttp } from '../hooks/http.hook';
+import { AuthContext } from '../context/AuthContext'
 
 const DetailPage = () => {
+    const { token } = useContext(AuthContext)
+    const [link, setLink] = useState({})
+    const { request } = useHttp()
+    const linkId = useParams().id
+
+    const getLinkFromServer = useCallback(async () => {
+        try {
+            const FetchedLink = await request(
+                `/api/link/${linkId}`,
+                'GET',
+                null,
+                { Authorization: `Bearer ${token}` }
+            )
+            setLink(FetchedLink)
+        } catch (error) {
+        }
+    }, [token, linkId, request]
+
+    )
+
+    useEffect(() => {
+        getLinkFromServer()
+    }, [getLinkFromServer])
+
     return (
         <div>
-            Detail page
+            <LinkCard link={link} />
         </div>
     );
 }
