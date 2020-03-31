705b9f5dae13d8e14520f6debd019d8ed6b4e248
Sergey Komarov <anaxronik@gmail.com>
2020-03-31 14:43:28 +0500

    create and add Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+
+
+export default function Navbar() {
+    const auth = useContext(AuthContext)
+    const history = useHistory()
+
+    const logoutHandler = (event) => {
+        event.preventDefault()
+        auth.logout()
+        history.push('/')
+    }
+
+    return (
+        <nav>
+            <div class="nav-wrapper blue darken-1">
+                <NavLink to="/" className='brand-logo'>Сокращение ссылок</NavLink>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><NavLink to='/create' >Создать</NavLink ></li>
+                    <li><NavLink to='/links' >Ссылки</NavLink ></li>
+                    <li><a href="/" onClick={logoutHandler}>Выйти</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
