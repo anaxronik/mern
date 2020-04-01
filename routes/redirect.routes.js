0891b521983d1fc4ccf5003ce67eca69b9c1210a
Sergey Komarov <anaxronik@gmail.com>
2020-04-01 14:04:35 +0500

    create redirect module

---
+++ routes/redirect.routes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const Link = require('../models/Link')
+const router = Router()
+
+router.get('/:code', async (req, res) => {
+    try {
+        const link = await Link.findOne({ code: req.params.code })
+        if (link) {
+            link.cliks++
+            await link.save()
+            return res.redirect(link.longLink)
+        }
+        res.status(404).json({ message: 'Ссылка не найдена' })
+    } catch (error) {
+        res.status(500).json({ message: 'Что-то пошло не так при редиректе' })
+    }
+})
+
+
+module.exports = router
